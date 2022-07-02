c362e915bfae40cfdf43b49c7dce215ca06dbd5a
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-02 09:22:37 -0300

    Update db name

--- gcp-main.tf
+++ gcp-main.tf
@@ -10,12 +10,12 @@ resource "google_artifact_registry_repository" "python-gcp-cloud" {
 }
 
 resource "google_sql_database" "database" {
-  name     = "python-database"
+  name     = "pytask"
   instance = google_sql_database_instance.instance.name
 }
 
 resource "google_sql_database_instance" "instance" {
-  name             = "python-instance"
+  name             = "mysql-db-instance"
   region           = "us-central1"
   database_version = "MYSQL_8_0"
   settings {
