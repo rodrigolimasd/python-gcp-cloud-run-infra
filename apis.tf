fb36131a31a027276e8fabd5da1f9b853c606b88
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-01 21:56:59 -0300

    hello img - adjusts

---
+++ apis.tf
@@ -0,0 +1,29 @@
+# Enable Artifact Registry API
+# resource "google_project_service" "artifactregistry" {
+#   provider = google-beta
+#   service = "artifactregistry.googleapis.com"
+#   disable_on_destroy = false
+# }
+
+# # Enable Cloud Run API
+# resource "google_project_service" "cloudrun" {
+#  project = "rds-labdevopscloud"
+#  service = "run.googleapis.com"
+#  disable_on_destroy = false
+# }
+
+# # Enable Cloud Resource Manager API
+# resource "google_project_service" "resourcemanager" {
+#   project = "rds-labdevopscloud"
+#   service  = "cloudresourcemanager.googleapis.com"
+#   disable_on_destroy = false
+# }
+
+# resource "time_sleep" "wait_30_seconds" {
+#   create_duration = "60s"
+#   depends_on = [
+#     google_project_service.artifactregistry,
+#     google_project_service.cloudrun,
+#     google_project_service.resourcemanager
+#     ]
+# }
--- gcp-main.tf
+++ gcp-main.tf
@@ -1,32 +1,3 @@
-# Enable Artifact Registry API
-# resource "google_project_service" "artifactregistry" {
-#   provider = google-beta
-#   service = "artifactregistry.googleapis.com"
-#   disable_on_destroy = false
-# }
-
-# # Enable Cloud Run API
-# resource "google_project_service" "cloudrun" {
-#  project = "rds-labdevopscloud"
-#  service = "run.googleapis.com"
-#  disable_on_destroy = false
-# }
-
-# # Enable Cloud Resource Manager API
-# resource "google_project_service" "resourcemanager" {
-#   project = "rds-labdevopscloud"
-#   service  = "cloudresourcemanager.googleapis.com"
-#   disable_on_destroy = false
-# }
-
-# resource "time_sleep" "wait_30_seconds" {
-#   create_duration = "60s"
-#   depends_on = [
-#     google_project_service.artifactregistry,
-#     google_project_service.cloudrun,
-#     google_project_service.resourcemanager
-#     ]
-# }
 
 # Create Artifact Registry Repository for Docker containers
 resource "google_artifact_registry_repository" "python-gcp-cloud" {
@@ -43,7 +14,7 @@ resource "google_artifact_registry_repository" "python-gcp-cloud" {
 resource "google_cloud_run_service" "python-gcp-cloud" {
   name = "python-gcp-cloud"
   location = "us-central1"
-  autogenerate_revision_name = true
+  
   metadata {
     annotations = {
       "run.googleapis.com/client-name" = "terraform",
@@ -60,7 +31,7 @@ resource "google_cloud_run_service" "python-gcp-cloud" {
   template {
     spec {
       containers {
-        image = "us-central1-docker.pkg.dev/rds-labdevopscloud/pythongcpcloud/python-gcp-cloud"
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
       }
     }
   }
@@ -69,13 +40,13 @@ resource "google_cloud_run_service" "python-gcp-cloud" {
 }
 
 # Allow unauthenticated users to invoke the service
-resource "google_cloud_run_service_iam_member" "run_all_users" {
-  service  = google_cloud_run_service.python-gcp-cloud.name
-  location = google_cloud_run_service.python-gcp-cloud.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
+# resource "google_cloud_run_service_iam_member" "run_all_users" {
+#   service  = google_cloud_run_service.python-gcp-cloud.name
+#   location = google_cloud_run_service.python-gcp-cloud.location
+#   role     = "roles/run.invoker"
+#   member   = "allUsers"
+# }
 
-output "cloud_run_instance_url" {
-  value = google_cloud_run_service.python-gcp-cloud.status[0].url
-}
+# output "cloud_run_instance_url" {
+#   value = google_cloud_run_service.python-gcp-cloud.status[0].url
+# }
